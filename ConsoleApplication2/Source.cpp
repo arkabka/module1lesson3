67cf6cdac9a6510e4d2c17c426d29e39edfe6e51
аркабека <@242_12>
2018-05-22 10:00:47 +0600

    Add project files.

---
+++ ConsoleApplication2/Source.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+void main()
+
+{
+	printf("a) b5 10 \tb) 100 t \tc) x 25\n   7 cm \t   1949 v \t   x y\n");
+
+
+}
---
+++ ConsoleApplication2/Source1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main()
+{ setlocale(LC_ALL, "Rus");
+
+
+float a;
+printf("Введите значение А");
+scanf()
+
+}
